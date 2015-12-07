7c0a77e312ece59bcbf4ca1356580bab1050fe34
sitay123 <sitay123@gmail.com>
2015-12-08 00:22:57 +0200

    fix bug for video

---
+++ war/js/Configuration/FIlters/trustUrl.js
@@ -0,0 +1,8 @@
+/**
+ * Created by itay on 12/7/2015.
+ */
+angular.module('myApp.filters').filter("trustUrl", ['$sce', function ($sce) {
+    return function (recordingUrl) {
+        return $sce.trustAsResourceUrl(recordingUrl);
+    };
+}]);
