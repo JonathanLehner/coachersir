0941336dc6ade475be9e39e562632246f9f792f8
sitay123 <sitay123@gmail.com>
2016-01-04 00:12:57 +0200

    add some features

--- war/js/configuration/filters/trustUrl.js
+++ war/js/configuration/filters/trustUrl.js
@@ -5,4 +5,4 @@ angular.module('myApp.filters').filter("trustUrl", ['$sce', function ($sce) {
     return function (recordingUrl) {
         return $sce.trustAsResourceUrl(recordingUrl);
     };
-}])
+}]);
