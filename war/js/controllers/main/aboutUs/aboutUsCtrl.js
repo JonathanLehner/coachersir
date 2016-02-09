33f91a7b04375447aaaa9078cc7f216e120a3bbc
Ronny Roktel <ronny.roktel@gmail.com>
2016-02-09 23:22:43 +0200

    about us designed

--- war/js/controllers/main/aboutUs/aboutUsCtrl.js
+++ war/js/controllers/main/aboutUs/aboutUsCtrl.js
@@ -1,6 +1,6 @@
 angular.module('myApp.controllers.main')
-    .controller('aboutUsCtrl',['$scope',function($scope)
-    {
+    .controller('aboutUsCtrl',['$scope','$translate',
+                       function($scope , $translate){
+    	$scope.listObjectives = $translate.instant("Menu.AboutUsContent.Objectives.List");
 
-    }
-]);
+    }]);
