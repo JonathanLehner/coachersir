3ae50761e349eec365932b3e19bc2e39e385be92
sitay123 <sitay123@gmail.com>
2016-03-01 20:41:15 +0200

    add filter before new branch

--- war/js/configuration/filters/filters.js
+++ war/js/configuration/filters/filters.js
@@ -90,6 +90,101 @@ angular.module('myApp.filters')
             return name;
         };
     }])
+    .filter('filterByDistanceLocation',function(){
+        "use strict";
+        return function(coaches,location,maxDistance){
+
+            if(maxDistance === undefined || maxDistance ===null){
+                maxDistance = 50;
+            }
+
+            var resultCoaches = [];
+            if(coaches && location !== undefined & location !== null){
+                var filterByLocation = function(location){
+                    var compLocation = new google.maps.LatLng(location.latitude, location.longitude);
+                    return function(coach) {
+                        var coachLocation = new google.maps.LatLng(coach.location.latitude, coach.location.longitude);
+                        var distance = google.maps.geometry.spherical.computeDistanceBetween(coachLocation,compLocation);
+                        return distance < maxDistance;
+                    }
+                };
+
+                resultCoaches = coaches.filter(filterByLocation(location));
+            }
+
+            return resultCoaches;
+        }
+    })
+    .filter('filterByRegion',function(){
+        "use strict";
+        return function(coaches,state){
+        }
+    })
+    .filter('filterByCity',function(){
+        "use strict";
+        return function(coaches,state){
+        }
+    })
+    .filter('filterByPrice',function(){
+        "use strict";
+        return function(coaches,price){
+
+            var resultCoaches = [];
+            if(coaches && price !== undefined & price !== null){
+                var filterByPrice = function(price){
+                    return function(coach) {
+                        return coach.price_per_hour < price
+                    }
+                };
+
+                resultCoaches = coaches.filter(filterByPrice(price));
+            }
+
+            return resultCoaches;
+        }
+    })
+    .filter('filterByDegrees',function(){
+        "use strict";
+        return function(coaches,degrees){
+
+            var resultCoaches = [];
+            if(coaches && degrees){
+
+                var filterByDegrees = function(degrees){
+                    return function(coach) {
+                        return degrees.some(function(degree){
+                            return coach.degrees.indexOf(degree) > -1
+                        })
+                    }
+                };
+
+                resultCoaches = coaches.filter(filterByDegrees(degrees));
+            }
+
+            return resultCoaches;
+        }
+    })
+    .filter('filterContentByTags',function(){
+        "use strict";
+        return function(contents,tags){
+
+            var resultContents = [];
+            if(contents && tags){
+
+                var filterContentByTags = function(tags){
+                    return function(content) {
+                        return tags.some(function(tag){
+                            return content.tags.indexOf(tag) > -1
+                        })
+                    }
+                };
+
+                resultContents = contents.filter(filterContentByTags(tags));
+            }
+
+            return resultContents;
+        }
+    })
      .filter('pagination', function(){
         return function(input, start) {
             start = parseInt(start, 10);
